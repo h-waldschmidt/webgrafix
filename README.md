fbaa0798718b508a83b8c697f1e0a7ac45f9f41b
Helmut Waldschmidt <helmut.waldschmidt@gmail.com>
2025-07-13 19:09:26 +0200

    update to new dawn version

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Webgrafix
 
 WebGPU Renderer
+`git submodule update --init`
--- src/webgrafix/Webgrafix.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace webgrafix {
-class Webgrafix {
-   public:
-    void setup();
-};
-}  // namespace webgrafix
